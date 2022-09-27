4ddc3e119b19b076f1ab69cbd9c5e287efff335d
esakkiraja-k <esakkirajait@gmail.com>
2022-09-27 13:38:33 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 7, 11, 15 };
+            int target = 9;
+            var results = new Solution().TwoSum(array,target);
+
+            for (int i = 0; i < results.Length; i++)
+            {
+                Console.WriteLine(i);
+            }
+           
+        }
+    }
+}
---
+++ TwoSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                {
+                    return new int[] { dic[nums[i]], i };
+                }
+                else
+                    dic[target - nums[i]] = i;
+            }
+
+            return new int[] { };
+        }
+    }
+}
