8959e1a4ec771613cddad2ba13d0126a9abf38ae
esakkiraja-k <esakkirajait@gmail.com>
2022-09-28 13:25:10 +0530

    daily task

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
+            /* TwoSums
             int[] array = { 2, 7, 11, 15 };
             int target = 9;
             var results = new Solution().TwoSum(array,target);
@@ -13,8 +14,13 @@ namespace LeetCode
             for (int i = 0; i < results.Length; i++)
             {
                 Console.WriteLine(i);
-            }
-           
+            }*/
+        //l1 = [2,4,3], l2 = [5,6,4]
+         ListNode l1 = new ListNode(2,new ListNode(4,new ListNode(3)));
+         ListNode l2 = new ListNode(5,new ListNode(6,new ListNode(4)));
+
+            ListNode result = new AddTwoNumbersSolution().AddTwoNumbers(l1,l2);
+                       
         }
     }
 }
